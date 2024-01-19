d224f8ad2ee20c22f10bbc9fa154cad2d3df8e87
HendrikdeJong <Hendrik.dejong06@gmail.com>
2024-01-19 18:13:26 +0100

    Animation to player, UI change

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour{
+
+    public int totalcoins;
+
+    public TextMeshProUGUI GameTimerText;
+    public float GameTimer = 120;
+    public Image timerImage;
+
+    public Image[] heartImages;
+    public Sprite fullHeartSprite;
+    public Sprite emptyHeartSprite;
+
+
+    public TextMeshProUGUI coinText;
+    
+
+    public static GameManager manager;
+
+    private void Awake() {
+        manager = this;
+    }
+
+    private void Update() {
+        coinText.text = totalcoins.ToString(); 
+
+        HandleGameTime();
+    }
+
+    private void HandleGameTime(){
+        GameTimer -= Time.deltaTime;
+        if(GameTimer < 0){
+            Debug.Log("game over by time");
+        }
+        GameTimerText.text = math.round(GameTimer).ToString();
+        timerImage.fillAmount = GameTimer / 120;
+    }
+
+    public void AddCoin(){
+        totalcoins++;
+    }
+
+    public void UpdateHealthUI(int currentHealth){
+        for (int i = 0; i < heartImages.Length; i++)
+        {
+            if (i < currentHealth){
+                // Display full heart for remaining health
+                heartImages[i].sprite = fullHeartSprite;
+            }else{
+                // Display empty heart for lost health
+                heartImages[i].sprite = emptyHeartSprite;
+            }
+        }
+    }    
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour{
@@ -7,34 +8,52 @@ public class Player : MonoBehaviour{
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private int health = 3;
+    
         
     private float resetinvistimer = 3f;
     private float invisTimer;
 
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private SpriteRenderer spriteRenderer;
     
     private Vector2 move;
     [SerializeField] private bool isGrounded;
 
     void Start(){
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update(){
         float sideMovement = Input.GetAxis("Horizontal");
         move = new Vector2(sideMovement, rb.velocity.y);
+        animator.SetFloat("MoveSpeed", math.abs(sideMovement));
 
         // Check for jump input
         if (Input.GetButtonDown("Jump") && isGrounded) {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        //animator
+        animator.SetBool("IsGrounded", isGrounded);
+        animator.SetFloat("Velocity", math.abs(rb.velocity.x / 5));
+
+        if(move.x < 0){
+            spriteRenderer.flipX = true;
+        }else
+            spriteRenderer.flipX = false;
+            
+        
+        //sprint
         if (Input.GetButton("Fire3")){
             speed = 7f;
-        }else{
+        }else
             speed = 5f;
-        }
+        
+        //timer
         invisTimer -= Time.deltaTime;
     }
 
@@ -47,7 +66,7 @@ public class Player : MonoBehaviour{
             PlayerHit();
         }
         if(other.CompareTag("Coin")){
-            CoinManager.manager.AddCoin();
+            GameManager.manager.AddCoin();
             Destroy(other.gameObject);
         }
     }
@@ -63,13 +82,13 @@ public class Player : MonoBehaviour{
     private void PlayerHit(){
         if(invisTimer < 0){
             invisTimer = resetinvistimer;
-            if(health < 1){
-                Debug.Log("player is dead!");
+        }
+
+        if(health < 1){
+                Debug.Log("player is dead");
             }else
                 health --;
-                Debug.Log(health);
-        }
-        
+                GameManager.manager.UpdateHealthUI(health);
     }
 
 }
